9e9bd991f3fdfd254fc8328a7f9f6178d74771a8
keyboarder-yang <15034009501@163.com>
2022-07-03 03:56:49 +0800

    gx

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import {createPureUI} from 'sheep-design-vue'
+import 'sheep-design-vue/dist/style.css'
+createApp(App).use(createPureUI()).mount('#app')
