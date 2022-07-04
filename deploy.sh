b3ef92b1dcfba2782c1572d43a6a245cd59fb9dc
yanghong <15034009501@163.com>
2022-07-04 12:56:08 +0800

    部署

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,29 @@
+
+#!/usr/bin/env sh
+
+# abort on errors
 set -e
+
 # build
 npm run build
 
-# 删除远程分支
-git push origin --delete gh-pages
+# navigate into the build output directory
+#cd docs/.vitepress/dist
 
-git subtree push --prefix dist origin gh-pages
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+# 删除远程分支
+#git push origin --delete gh-pages
+git init
+git add -A
+git commit -m '部署'
+#git subtree push --prefix dist origin gh-pages
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# just a example, please change the follow line with your github account!!!
+git push -f  https://github.com/keyboarder-yang/vitepress-blog master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 
 cd -
