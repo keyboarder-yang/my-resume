43a749acc05ec8e671d8467375be76613ee2a52c
keyboarder-yang <15034009501@163.com>
2022-07-05 23:02:28 +0800

    gx

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
-  <div>
-    简历
+  <div v-if="_isMobile()">
+    当前页面已在手机端打开
+  </div>
+  <div v-else>
+    当前页面已在PC端打开
   </div>
 </template>
+<script lang="ts" setup>
+const _isMobile = () => {
+  return navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i);
+}
+</script>
 <style scoped>
 
 </style>
