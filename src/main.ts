7bbca2b95515cc7c9b180e2df03bbf0c055e257c
yanghong <15034009501@163.com>
2022-09-03 18:08:55 +0800

    更新

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import {createPureUI} from 'sheep-design-vue'
 import 'animate.css';
 import './css/common.css'
-import 'sheep-design-vue/dist/style.css'
-createApp(App).use(createPureUI()).mount('#app')
+import './assets/font.css'
+createApp(App).mount('#app')
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,54 +1,125 @@
 <template>
-  <div class="container">
-    <div class="header">
-      <div class="logo">
-        <img src="../../public/logo.png" width="40px">
+  <div class="container-fluid">
+    <!-- Start 顶部导航栏 -->
+    <div class="nav">
+      <div class="nav-contents">
+        <div class="nav-logo-area">
+          <img src="../assets/logo.png" alt="个人简历" width="40">
+          <span class="nav-logo-text">YangHong</span>
+        </div>
+        <div class="nav-bars-area">
+          <div class="nav-bar-item" v-for="(nav,index) in navBars" @click="handleNavClick(index)">
+            <a class="nav-bar-item-label" :href="nav.href" :class="{active: currentIndex===index}">{{ nav.name }}</a>
+          </div>
+        </div>
       </div>
-      <div class="options">1</div>
-      <div class="options">2</div>
-      <div class="options">3</div>
-      <div class="options">4</div>
     </div>
-    <div class="content"></div>
+    <!-- End 顶部导航栏 -->
+
+    <div class="test" :id="item.id" v-for="(item,index) in navBars">{{item.name}}</div>
   </div>
 </template>
 
 <script lang='ts' setup>
+import {ref} from 'vue'
+
 /**
  *@Author: YangHong
- *@CreateDate: 2022/7/30
- *@LastModifiedDate: 2022/7/30 16:56
- *@Description: 个人简历首页
+ *@CreateDate: 2022/8/13
+ *@LastModifiedDate: 2022/8/13 13:58
+ *@Description:
  */
-
+const navBars = ref([
+  {name: '主页',id:'home',href:'#home'},
+  {name: '个人技能',id:'skill',href:'#skill'},
+  {name: '项目经历',id:'experience',href:'#experience'},
+  {name: '关于我',id:'about',href:'#about'},
+  {name: '联系我',id:'contact',href:'#contact'},
+])
+const currentIndex = ref(0)
+const handleNavClick = (index:number) => {
+  currentIndex.value = index
+}
 </script>
 
-<style scoped>
-.container {
-  color: white;
+<style scoped lang="less">
+@primary-color: #ED7457;
+@primary-color-transparent: rgba(237,116,87,.3);
+.nav {
+  position: sticky;
+  top: 0;
+  box-shadow: 0 5px 5px 0 #dddddd;
+  padding: 5px 60px;
   box-sizing: border-box;
-  min-height: 100vh;
-  background-image: linear-gradient(to right, rgba(10, 31, 52, 0.8), rgba(255, 255, 255, 0.3));
-  display: grid;
-  grid-template-rows: 60px 1fr;
-  grid-template-columns: 1fr;
+  font-family: 'resume';
+  .nav-contents {
+    width: 100%;
+    height: 60px;
+    line-height: 60px;
+    display: flex;
+
+    .nav-logo-area {
+      flex: 1;
+      .nav-logo-text{
+        display: inline-block;
+        margin-left: 40px;
+        font-weight: bolder;
+      }
+
+      img{
+        vertical-align: middle;
+      }
+    }
+
+    .nav-bars-area {
+      flex: 1;
+      display: flex;
+
+      .nav-bar-item {
+        flex: 1;
+        text-align: center;
+        user-select: none;
+        cursor: pointer;
+
+        .nav-bar-item-label {
+          box-sizing: border-box;
+          display: inline-block;
+          height: 100%;
+          min-width: 60%;
+          position: relative;
+          letter-spacing: 3px;
+          text-decoration: none;
+          color: black;
+        }
+        .nav-bar-item-label:hover{
+          font-weight: bolder;
+          color: @primary-color;
+        }
+        .nav-bar-item-label:before {
+          position: absolute;
+          bottom: -4px;
+          left: 0;
+          content: '';
+          height: 4px;
+          width: 0%;
+          transition: all .5s;
+          background-color: @primary-color;
+        }
+        .nav-bar-item-label:hover:before {
+          width: 100%;
+        }
+      }
+
+    }
+  }
 }
-.header{
-  display: grid;
-  grid-template-columns: 3fr repeat(4,1fr);
-  grid-template-rows: 1fr;
-  background-color: rgba(0,0,0,.3);
-}
-.logo{
-  padding-left: 40px;
-  font-size: 20px;
+.active{
+  border-bottom: 4px solid @primary-color;
+  color: @primary-color;
   font-weight: bolder;
-  align-self: center;
 }
-.logo img{
-  vertical-align: middle;
-}
-.options{
-  align-self: center;
+
+.test{
+  height: 800px;
 }
 </style>
